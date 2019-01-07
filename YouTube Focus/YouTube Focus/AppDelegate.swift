32eb9f470225aa9e7f3bd3697a8543a18919388f
Justin Zhu <justinthezhu@gmail.com>
2019-01-06 20:10:02 -0500

    Added remote to project

---
+++ YouTube Focus/YouTube Focus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  YouTube Focus
+//
+//  Created by Justin Zhu on 1/6/19.
+//  Copyright © 2019 Justin Zhu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
